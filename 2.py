8503600635c964cd89acf69210cc28aa31b607d1
wolfista <83009717+wolfista@users.noreply.github.com>
2021-05-09 23:20:39 +0200

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import csv
+# badges_comma = np.loadtxt('badges-five-header.txt', delimiter = ',', skiprows=1)
+# print(badges_comma.dtype)
+
+# def increase(id):
+#     return int(id) + 1000
+
+# increased = np.loadtxt('badges-five.txt', delimiter=',', dtype=int, converters={0: increase})
+# print(type(increased))
+
+csv.list_dialects()
+with open(users)
